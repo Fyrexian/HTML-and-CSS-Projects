c849facbf3b5d92376ad333e1d6c0ba6c101b86b
Fyrexian <vaethconner@gmail.com>
2024-04-14 12:15:23 -0500

    Update Hello_World.js

--- NotePad++ Assignment/Java_Test/Hello_World.js
+++ NotePad++ Assignment/Java_Test/Hello_World.js
@@ -1,2 +1,8 @@
 var greeting = "Hello, World!";
 console.log(greeting);
+
+var greeting2 = "Welcome to Node!";
+console.log(greeting2);
+
+var sentence1 = "Learning to code with debugger!";
+console.log(sentence1);
